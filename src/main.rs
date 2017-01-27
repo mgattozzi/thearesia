a8024a4e0b2d6a6c36b04ab7c94f24f3fcd5bc3a
Alex Crichton <alex@alexcrichton.com>
2017-01-27 00:00:06 -0800

    Avoid `NewService` manually through closures
    The `NewService` trait is auto-implemented for closures, so it should be
    slightly more ergonomic to just make a closure to create our services.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ extern crate serde_json;
 
 use futures::Future;
 use futures::Stream;
-use hyper::server::{Service, NewService, Http};
+use hyper::server::{Service, Http};
 use hyper::server;
 use hyper::error;
 use hyper::Method::Post;
@@ -20,7 +20,7 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 fn main() {
     let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
     let _ = Http::new()
-        .bind(&socket, Webhook)
+        .bind(&socket, || Ok(Webhook))
         .map(|server| server.run())
         .map_err(|e| println!("Server failed to setup: {}", e));
 }
@@ -67,15 +67,5 @@ impl Service for Webhook {
             futures::finished(res).boxed()
 
         }
-    }
-}
-
-impl NewService for Webhook {
-    type Request = server::Request;
-    type Response = server::Response;
-    type Error = error::Error;
-    type Instance = Webhook;
-    fn new_service(&self) -> Result<Self::Instance, std::io::Error> {
-        Ok(Webhook)
     }
 }
