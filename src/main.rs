ab47a061b2ec3b6df0074ec7d8e9923a0aff3f06
Michael Gattozzi <mgattozzi@gmail.com>
2017-01-26 19:45:47 -0500

    Begin handling requests from GitHub
    This does a few things:
    
    1. First it makes sure that we only recieve POST requests
    2. It then gets the body into a single vector of bytes if it is there
    3. If the body has bytes it then deserializes them from JSON
       and prints them out to the console
    
    More work will need to be done but this is a good start in terms of
    dealing with requests.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
-// A tiny async echo server with tokio-core
+//! Thearesia a tool to maintain GitHub Repos and Organizations
+#![cfg_attr(feature = "dev", plugin(clippy))]
+#![cfg_attr(feature = "dev", plugin)]
 
 extern crate hyper;
 extern crate futures;
+extern crate serde;
+extern crate serde_json;
 
 use futures::Future;
+use futures::Stream;
 use hyper::server::{Service, NewService, Http};
 use hyper::server;
 use hyper::error;
-
+use hyper::Method::Post;
+use hyper::status::StatusCode::MethodNotAllowed;
+use serde_json::Value;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 fn main() {
     let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
-    Http::new()
+    let _ = Http::new()
         .bind(&socket, Webhook)
-        .unwrap()
-        .run()
-        .unwrap()
+        .map(|server| server.run())
+        .map_err(|e| println!("Server failed to setup: {}", e));
 }
 
 struct Webhook;
@@ -27,9 +33,40 @@ impl Service for Webhook {
     type Error = error::Error;
     type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
     fn call(&self, req: server::Request) -> Self::Future {
-        let resp = server::Response::new();
-        println!("I have a connection");
-        futures::finished(resp).boxed()
+        let (method, _, _, _headers, body) = req.deconstruct();
+
+        // Make sure we only recieve POST requests from Github
+        if method == Post {
+
+            // Get all of the chunks streamed to us in our request
+            // GitHub gives us a lot of data so there might be
+            // more than one Chunk
+            body.collect()
+                // Then put them all into a single buffer for parsing
+                .and_then(move |chunk| {
+                    let mut buffer: Vec<u8> = Vec::new();
+                    for i in chunk {
+                        buffer.append(&mut i.to_vec());
+                    }
+                    Ok(buffer)
+                })
+                // If there is JSON do things with it
+                // Send to the server that we got the data
+                .map(move |buffer| {
+                    if !buffer.is_empty() {
+                        println!("{:#?}", serde_json::from_slice::<Value>(&buffer).unwrap());
+                    }
+
+                    server::Response::new()
+                }).boxed()
+
+        } else {
+
+            let mut res = server::Response::new();
+            res.set_status(MethodNotAllowed);
+            futures::finished(res).boxed()
+
+        }
     }
 }
 
